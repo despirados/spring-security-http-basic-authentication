1ee1ed26b84151748ca2e4a69692d372ba8b9247
despirados <ayed.hedi@gmail.com>
2015-12-11 21:59:50 +0100

    done!

--- src/main/java/com/srccodes/spring/controller/ApiController.java
+++ src/main/java/com/srccodes/spring/controller/ApiController.java
@@ -1,8 +1,28 @@
 package com.srccodes.spring.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by hedi on 12/11/15.
  */
-@Rest
+@RestController(value = "/api")
 public class ApiController {
+
+    /**
+     * parameter to send with the header: Basic dGVzdDp0ZXN0
+     *
+     */
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody String helloWorldGet() {
+        return "Hello World !!";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody String helloWorldPost() {
+        return "Hello World !!";
+    }
 }
--- src/main/java/com/srccodes/spring/controller/SpringSecurityHelloController.java
+++ src/main/java/com/srccodes/spring/controller/SpringSecurityHelloController.java
@@ -12,17 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpringSecurityHelloController {
 
-	@RequestMapping("/public")
+	@RequestMapping("/test")
 	public String accessPublicPage(Model model) {
-		model.addAttribute("message", "This page is publicly accessible. No authentication is required to view.");
+		model.addAttribute("message", "Hello World !!");
 
 		return "public";
 	}
-	
-	@RequestMapping("/secured/mypage")
-	public String accessSecuredPage(Model model) {
-		model.addAttribute("message", "Only you are authenticated and authorized to view this page.");
 
-		return "/secured/mypage";
-	}
 }
